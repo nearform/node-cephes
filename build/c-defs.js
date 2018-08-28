cbca1615bd57680fa7803d039abcdfd9f8eb5f80
Andreas Madsen <amwebdk@gmail.com>
2018-08-28 14:08:48 +0200

    remove unused c-defs

--- build/c-defs.js
+++ build/c-defs.js
@@ -2,9 +2,6 @@
 // The real functions are defined in cephes.js, this is just so
 // emscripten doesn't complain.
 mergeInto(LibraryManager.library, {
-  __errno_location: function (pointer) {
-    return pointer;
-  },
   mtherr: function(name /* char* */, code /* int */) {
     throw new Error(name);
   }
