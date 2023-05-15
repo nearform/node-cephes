c144bba38c2486d0c715cea7a150acbcd758fa70
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2023-05-15 09:06:22 -0400

    ensure Buffer is available on self/window

--- cephes-wrapper.js
+++ cephes-wrapper.js
@@ -3,6 +3,7 @@ const TOTAL_STACK = 1024 * 1024; // 1MB
 const TOTAL_MEMORY = 2 * 1024 * 1024; // 1MB
 const WASM_PAGE_SIZE = 64 * 1024; // Defined in WebAssembly specs
 
+if (self && !self.Buffer) self.Buffer = require('buffer').Buffer;
 const WASM_CODE = Buffer.from(require('./cephes.wasm.base64.json'), 'base64');
 
 class CephesWrapper {
