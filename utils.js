214f0a5d1f4b3048087c03fd0a5bc2bbf3f35648
Giovanni Bucci <giovanni.bucci@nearform.com>
2024-04-26 10:23:57 +0200

    added the missing utils

---
+++ utils.js
@@ -0,0 +1,12 @@
+const errorMappings = {
+    1: 'argument domain error',
+    2: 'function singularity',
+    3: 'overflow range error',
+    4: 'underflow range error',
+    5: 'total loss of precision',
+    6: 'partial loss of precision',
+    33: 'Unix domain error code',
+    34: 'Unix range error code',
+}
+
+module.exports = {errorMappings};
