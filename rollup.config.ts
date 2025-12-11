fcc52bae476b1acfa036b07cacd0273790e95eed
Mahdi Lamb <mahdilamb@gmail.com>
2025-12-11 07:51:46 +0000

    chore(rollup): move to typescript

---
+++ rollup.config.ts
@@ -0,0 +1,21 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "index.js",
+  output: {
+    file: "index.mjs",
+    format: "es",
+  },
+  plugins: [
+    nodePolyfills(),
+    resolve({
+      preferBuiltins: false,
+      browser: true,
+    }),
+    commonjs(),
+    json(),
+  ],
+}
